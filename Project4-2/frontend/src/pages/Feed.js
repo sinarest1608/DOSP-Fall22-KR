387c882488f06d8e61090bdfe1bfa9a585dc9233
Rajdev Kapoor <rajdev1409@gmail.com>
2022-12-15 13:22:11 -0500

    ui fix

--- Project4-2/frontend/src/pages/Feed.js
+++ Project4-2/frontend/src/pages/Feed.js
@@ -1,53 +1,49 @@
-import { Button, Container, FormControlLabel, FormGroup, Grid, Switch, Typography } from "@mui/material"
-import { useState } from "react"
-import Appbar from "../components/Appbar"
-import Tweet from "../components/Tweet"
-import { getAllTweets } from "../data/LocalStorageDB"
+import { Button, Container, FormControlLabel, FormGroup, Grid, Switch, Typography } from '@mui/material';
+import { useState } from 'react';
+import Appbar from '../components/Appbar';
+import Tweet from '../components/Tweet';
+import { getAllTweets } from '../data/LocalStorageDB';
 
 const FeedPage = () => {
-    
-    const [tweets, setTweets] = useState(getAllTweets())
+	const [ tweets, setTweets ] = useState(getAllTweets());
 
-    return <Container className="page">
-        <Appbar />
-        <Container maxWidth="lg" className="page-content">
-            <br />
-            <Grid container spacing={2}>
-                <Grid item xs={9}>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}>
-                        Your Feed
-                    </Typography>
-                    <br/>
-                {
-                    tweets.length === 0 ? 'No twits that match the current query. Maybe create one?' : null
-                }
-                </Grid>
-                <Grid item xs>
-                    <FormGroup>
-                        <FormControlLabel control={<Switch />} label="Subscribed only" />
-                    </FormGroup>
-                </Grid>
-                {
-                    tweets.map(tweet => {
-                        return <Grid item xs={12}>
-                            <Tweet content={tweet.content} createdAt={tweet.createdAt} user={tweet.user} />
-                        </Grid>
-                    })
-                }
-            </Grid>
-        </Container>
-    </Container>
+	return (
+		<Container className="page">
+			<Appbar />
+			<Container maxWidth="lg" className="page-content">
+				<br />
+				<Grid container spacing={2}>
+					<Grid item xs={9}>
+						<Typography
+							variant="h6"
+							noWrap
+							sx={{
+								display: { xs: 'none', md: 'flex' },
 
-}
+								textDecoration: 'none'
+							}}
+						>
+							Your Feed
+						</Typography>
+						<br />
+						{tweets.length === 0 ? "It's Lonely out there!" : null}
+					</Grid>
+					<Grid item xs>
+						<FormGroup>
+							<FormControlLabel control={<Switch />} label="Subscribed only" />
+						</FormGroup>
+					</Grid>
+					{tweets.map((tweet) => {
+						return (
+							<Grid item xs={12}>
+								<Tweet content={tweet.content} createdAt={tweet.createdAt} user={tweet.user} />
+							</Grid>
+						);
+					})}
+				</Grid>
+			</Container>
+		</Container>
+	);
+};
 
-export default FeedPage
+export default FeedPage;
--- Project4-2/frontend/src/pages/Login.js
+++ Project4-2/frontend/src/pages/Login.js
@@ -1,62 +1,68 @@
-import { Button, Container, TextField, Typography } from "@mui/material"
-import { useState } from "react"
-import { loginUser } from "../data/LocalStorageDB";
+import { Button, Container, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
+import { loginUser } from '../data/LocalStorageDB';
 
 const LoginPage = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+	const [ username, setUsername ] = useState('');
+	const [ password, setPassword ] = useState('');
 
-    const login = () => {
-        if(username !== '' && password !== '') {
-            const user = loginUser(username, password)
-            if (user) {
-                window.location.reload()
-            } else {
-                window.alert('Incorrect username or password')
-            }
-        }
-    }
-    return <Container maxWidth="xs" className="page">
-        <br />
-        <br />
-        <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-                mr: 2,
-                display: { xs: 'none', md: 'flex' },
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'inherit',
-                textDecoration: 'none',
-            }}>
-            TWIT
-        </Typography>
-        <br />
-        <TextField id="username"
-            label="Username"
-            variant="outlined"
-            fullWidth
-            onChange={(e) => setUsername(e.target.value)} />
-        <br />
-        <br />
-        <TextField
-            id="password"
-            label="Password"
-            variant="outlined"
-            type="password"
-            fullWidth
-            onChange={(e) => setPassword(e.target.value)} />
-        <br />
-        <br />
-        <Button variant="contained" onClick={login}>Login</Button>
-        <br />
-        <br />
-        <a href="/signup">New user? Sign Up</a>
-    </Container>
-}
+	const login = () => {
+		if (username !== '' && password !== '') {
+			const user = loginUser(username, password);
+			if (user) {
+				window.location.reload();
+			} else {
+				window.alert('Incorrect username or password');
+			}
+		}
+	};
+	return (
+		<Container maxWidth="xs" className="page" style={{ backgroundColor: '#1DA1F2' }}>
+			<br />
+			<br />
+			<Typography
+				variant="h5"
+				noWrap
+				component="a"
+				href="/"
+				sx={{
+					mr: 2,
+					display: { xs: 'none', md: 'flex' },
+					fontWeight: 1000,
+					color: 'inherit',
+					textDecoration: 'none'
+				}}
+			>
+				TWITERRRRRR!
+			</Typography>
+			<br />
+			<TextField
+				id="username"
+				label="Username"
+				variant="outlined"
+				fullWidth
+				onChange={(e) => setUsername(e.target.value)}
+			/>
+			<br />
+			<br />
+			<TextField
+				id="password"
+				label="Password"
+				variant="outlined"
+				type="password"
+				fullWidth
+				onChange={(e) => setPassword(e.target.value)}
+			/>
+			<br />
+			<br />
+			<Button variant="contained" onClick={login}>
+				Login
+			</Button>
+			<br />
+			<br />
+			<a href="/signup">New user? Sign Up</a>
+		</Container>
+	);
+};
 
-export default LoginPage
+export default LoginPage;
